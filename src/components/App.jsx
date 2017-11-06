0d48f6ba12931de29008f885e6f7dd64b6a1f750
Andreu Grimalt <andreugrimalt@users.noreply.github.com>
2017-11-06 16:07:37 +0000

    hooked with Firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
+/* eslint-disable react/forbid-prop-types,
+react/require-default-props,
+react/no-unused-prop-types
+*/
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'; // can also come from react if react <= 15.4.0
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firebaseConnect } from 'react-redux-firebase';
 
-function App() {
+const App = ({ entries }) => {
+  console.log(entries);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <h1>{entries}</h1>
     </div>
   );
-}
+};
 
-export default App;
+App.propTypes = {
+  entries: PropTypes.object,
+  firebase: PropTypes.object, // comes from firebaseConnect
+};
+
+export default compose(firebaseConnect(['entries']), connect(state => ({ entries: state.firebase.data.entries })))(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,38 @@
-import { combineReducers } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import { createStore, combineReducers, compose } from 'redux';
+import { reactReduxFirebase, firebaseStateReducer } from 'react-redux-firebase';
+import firebase from 'firebase';
 import entries from './entries';
 
-const spandWatchApp = combineReducers({
+const firebaseConfig = {
+  apiKey: 'AIzaSyBpb8tGo9YOAfVFY8h-QWG5QUHy0DYs-SI',
+  authDomain: 'spend-watch.firebaseapp.com',
+  databaseURL: 'https://spend-watch.firebaseio.com',
+  projectId: 'spend-watch',
+  storageBucket: 'spend-watch.appspot.com',
+  messagingSenderId: '832207920078',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const reduxFirebaseConfig = {
+  userProfile: 'users',
+  enableLogging: true,
+};
+
+// Add reactReduxFirebase store enhancer
+/* eslint-disable */
+const createStoreWithFirebase = compose(reactReduxFirebase(firebase, reduxFirebaseConfig))(createStore);
+/* eslint-enable */
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseStateReducer,
   entries,
 });
 
-export default spandWatchApp;
+// Create store with reducers and initial state
+const initialState = {};
+const store = createStoreWithFirebase(rootReducer, initialState);
+
+export default store;
